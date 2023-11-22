d302a40c17f9954b7faca9b0602a64ccaf1e5c5d
sardor <sshavkatov900@gmail.com>
2023-11-22 19:54:56 +0500

    create Models

--- statistic/models.py
+++ statistic/models.py
@@ -1,1 +1,48 @@
-from django.db import models
+from django.db.models import Model, CharField, DateField, PositiveIntegerField, ForeignKey, RESTRICT, CASCADE
+
+
+class Client(Model):
+    fullname = CharField(
+        'Полное имя клиента',
+        max_length=120)
+    birthdate = DateField('День рождение клиента')
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Employee(Model):
+    fullname = CharField(
+        'Полное имя клиента',
+        max_length=120
+    )
+    birthdate = DateField('День рождение сотрудника')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+
+class Product(Model):
+    name = CharField(
+        'Название продукта',
+        max_length=50
+    )
+    quantity = PositiveIntegerField()
+    price = PositiveIntegerField()
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Order(Model):
+    client = ForeignKey(Client, on_delete=RESTRICT)
+    product = ForeignKey(Product, on_delete=RESTRICT)
+    employee = ForeignKey(Employee, on_delete=CASCADE)
+    date = DateField('Время заказа')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
